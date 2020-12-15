53f42338cbdfbfa75b272be091f1b3e4f9c94820
WCIofQMandRA <zichen350@gmail.com>
2020-12-16 00:19:39 +0800

    Add copyright declaration

--- dialog_names.cpp
+++ dialog_names.cpp
@@ -1,4 +1,25 @@
+//dialog_names.cpp: 类dialog_names的实现
 //用户此前存档名字的选择与新名字的键入
+
+//	-* mode: C++		encoding:UTF-8 *-
+//	Copyright 2020 张子辰 & 吕航 (GitHub: WCIofQMandRA & LesterLv)
+//
+//	This file is part of the game 保卫行星
+//
+//	This game is free software; you can redistribute it and/or modify it
+//	under the terms of the GNU Lesser General Public License as published by
+//	the Free Software Foundation; either version 3, or (at your option) any
+//	later version.
+//
+//	This game is distributed in the hope that it will be useful, but
+//	WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+//	or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+//	License for more details.
+//
+//	You should have received copies of the GNU Lesser General Public License
+//	and the GNU Gerneral Public License along with 保卫行星 .
+//	If not, see https://www.gnu.org/licenses/.
+
 #include "dialog_names.hpp"
 #include "ui_dialog_names.h"
 #include <QListWidgetItem>
